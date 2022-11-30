1bde6b02373a8b35876b24672f3fc88f56727384
nabernal <america94bernal@gmail.com>
2022-11-29 22:53:26 -0800

    Updating app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,21 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+// Create function that builds a table
+function buildTable(data) {
+    tbody.html("");
+}
+
+
+// Create a forEach function
+data.forEach((dataRow) => {
+// create a variable that will append a row to the table body
+    let row = tbody.append("tr");
+// add code to loop through each field in the dataRow argument    
+    Object.values(dataRow).forEach((val) => {
+        // append each value of the object to a cell in the table
+        let cell = row.append("td");
+        cell.text(val);
+        }
+    );
+});
